5d1148bdc5c67e6fe41ae7081438ee006a6fa244
Vaibhav <vaibhavdeep46@gmail.com>
2020-09-25 15:59:24 +0530

    authUser changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
@@ -6,10 +6,27 @@ import Chat from "./Components/Chat/Chat";
 import Header from "./Components/Header/Header";
 import Login from "./Components/Login/Login";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 
 function App() {
   const [{ user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
 
   return (
     <div className="app">
